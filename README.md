9fa00bbbefd64271a75e895a2b0c7b5f903a5a06
MichalSkoula <michal@skoula.cz>
2024-10-21 22:01:33 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# github-avatars
+# GitHub Avatars (Firefox Extension)
 Increases the size of user avatars in GitHub issues and adds a name next to them.
+
+The problem with Github issues is that the avatars (especially if they are just auto-generated) are small and it is hard to distinguish between users.
+
+This extension doubles the size of the avatars and puts the names next to them.
+
+## Installation
+
+https://addons.mozilla.org/en-US/firefox/addon/github-avatars/
+
+## About
+* MIT Licence
+* https://skoula.cz
+
+
+<a href="https://www.buymeacoffee.com/mskoula"><img src="https://www.buymeacoffee.com/assets/img/guidelines/download-assets-sm-1.svg" height="40"></a>
+<a href="https://paypal.me/truehipstercz?country.x=CZ&locale.x=en_US"><img src="https://raw.githubusercontent.com/andreostrovsky/donate-with-paypal/master/blue.svg" height="40"></a>
---
+++ content.js
@@ -0,0 +1,31 @@
+function processAvatars() {
+    const avatarStacks = document.querySelectorAll('.AvatarStack-body');
+
+    avatarStacks.forEach((stack, index) => {
+        const avatarLinks = stack.querySelectorAll('a.avatar');
+
+        const names = [];
+
+        avatarLinks.forEach(avatarLink => {
+            const avatar = avatarLink.querySelector('img.avatar-user');
+            if (avatar) {
+                names.push(avatar.alt.replace('@', ''));
+            }
+        });
+
+        const concatenatedAltText = names.join(', ');
+
+        if (concatenatedAltText && !stack.querySelector('.avatar-alt-text')) {
+            // Create the alt text element
+            const altText = document.createElement('div');
+            altText.classList.add('avatar-alt-text');
+            altText.textContent = concatenatedAltText.trim();
+
+            // Insert the alt text before the first avatar link
+            stack.insertBefore(altText, avatarLinks[0]);
+        }
+    });
+}
+
+// Run the function when the page loads
+processAvatars();
